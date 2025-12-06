48ecfb40b266d8c31b7bf30b53f7b49080898203
Israel Moreira <israelmessias96@gmail.com>
2025-12-06 01:35:50 -0300

    Alteração da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-        return <h1>Amo a nossa família e tudo que estamos construindo.</h1>
+        return <h1>Melissa, eu te amo, amo a nossa família e tudo que estamos construindo.</h1>
 }
 
 export default Home;

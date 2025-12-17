cb66209773e257525e94144b4db49a3a0943956f
Israel Moreira <israelmessias96@gmail.com>
2025-12-16 23:41:58 -0300

    configuração do prettier e seus scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-  return <h1>Melissa, eu te amo profundamente. Amo a nossa família e tudo o que estamos construindo.</h1>
+function Home() {
+  return (
+    <h1>
+      Melissa, eu te amo profundamente. Amo a nossa família e tudo o que estamos
+      construindo.
+    </h1>
+  );
 }
 
 export default Home;
